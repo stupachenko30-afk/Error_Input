b53cb7ab7dd76b3563703671f7873003fe4c0708
Yevhenii <stupachenko30@gmail.com>
2026-06-16 19:27:25 +0200

    add1

--- src/main/java/HWMainProduct.java
+++ src/main/java/HWMainProduct.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class HWMainProduct {
@@ -6,5 +9,42 @@ public class HWMainProduct {
 
         HWProduct pr = getHWProduct();
         products.add(pr);
+
+        printProducts(products);
     }
+
+    private static void printProducts(ArrayList<HWProduct> products) {
+        for (HWProduct e : products) {
+            System.out.println(e);
+        }
+    }
+    private static HWProduct getHWProduct() {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        HWProduct pr = null;
+
+        try{
+
+            System.out.print("Enter name>>>");
+            String name = br.readLine();
+            System.out.print("Enter Code>>>");
+            String str = br.readLine();
+            int code = Integer.parseInt(str);
+            System.out.print("Enter Price>>>");
+            str = br.readLine();
+            double price = Double.parseDouble(str);
+            pr = new HWProduct(name, code,price);
+
+
+
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getClass().getSimpleName());
+        }catch (NumberFormatException e){
+        System.out.println("ERROR: " + e.getClass().getSimpleName());
+        }
+        return pr;
+    }
+
+
 }
+
